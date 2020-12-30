0d6d3a6d55b08bd620f03791750fefbfc450f3df
eigozhe <70949616+eigozhe@users.noreply.github.com>
2020-12-30 22:35:22 +0200

    Update igor zhelezko.py

--- igor zhelezko.py
+++ igor zhelezko.py
@@ -1,23 +1,27 @@
 import csv
 import re
+
 symbols = 0
 spaces = 0
 punctuation_symbols = 0
+br_count = 0
 words = 0
 sentences = 0
 with open('steam_description_data.csv', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
-        strings = ','.join(row)
-        symbols += len(strings)
-        spaces += strings.count(' ')
-        punctuation_symbols += strings.count(',') + strings.count('.') + strings.count('?') + strings.count('!')
-        punctuation_symbols += strings.count('\"') + strings.count('+"') + strings.count('\"') + strings.count(':') + strings.count(';')
-        punctuation_symbols += strings.count('-') + strings.count('(') + strings.count(')')
-        words += len(re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", strings))
-        sentences += len(re.findall(r"([A-Z][^.!?]*[.!?])", strings))
-print("Количество символов (общее):", symbols)
-print("Количество символов (без пробелов):", symbols - spaces)
-print("Количество символов (без знаков препинания):", symbols - punctuation_symbols)
-print("Количество слов:", words)
-print("Количество предложений:", sentences)
+        string = ','.join(row)
+        symbols += len(string)
+        spaces += string.count(' ')
+        br_count += string.count('<br>')
+        punctuation_symbols += string.count('.') + string.count(',') + string.count('!') + string.count('?')
+        punctuation_symbols += string.count( '\"' ) + string.count( '\'' ) + string.count( ':' ) + string.count( ';' )
+        punctuation_symbols += string.count('-') + string.count('(') + string.count(')') + string.count('<br>')
+        words += len( re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", string))
+        sentences += len( re.findall(r"([A-Z][^\.!?]*[\.!?])", string))
+print( "Количество символов (общее):", symbols)
+print( "Количество символов (без пробелов):", symbols - spaces )
+print( "Количество символов (без знаков препинания):", symbols - punctuation_symbols )
+print( "Количество слов:", words )
+print( "Количество предложений:", sentences )
+print( "Количество переводов строк", br_count)
